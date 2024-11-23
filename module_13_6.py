536f3da41ca8d177463b8a115e17798d2f7f2788
Juvikk <alex.kondor2002@mail.ru>
2024-11-24 01:05:44 +0300

    Update module_13_6.py

--- module_13_6.py
+++ module_13_6.py
@@ -1,59 +1,63 @@
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-import asyncio
 
 api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
-button1 = KeyboardButton(text='Рассчитать')
-button2 = KeyboardButton(text='Информация')
-kb.add(button1)
-kb.add(button2)
+button_1 = KeyboardButton(text='Рассчитать')
+button_2 = KeyboardButton(text='Информация')
+kb.add(button_1)
+kb.add(button_2)
 
-kb2 = InlineKeyboardMarkup()
-in_button1 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
-in_button2 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
-kb2.add(in_button1)
-kb2.add(in_button2)
+ikb = InlineKeyboardMarkup()
+i_button_1 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+i_button_2 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+ikb.add(i_button_1)
+ikb.add(i_button_2)
 
-class UserState (StatesGroup):
+
+class UserState(StatesGroup):
     age = State()
     growth = State()
     weight = State()
 
 
-@dp.message_handler(commands=['start'])
-async def start_message(message):
-    print('Привет! Я бот помогающий твоему здоровью.')
-    await message.answer("Привет! Я бот, помогающий твоему здоровью.", reply_markup=kb)
-
-@dp.message_handler(text = 'Рассчитать')
+@dp.message_handler(text='Рассчитать')
 async def main_menu(message):
-    await message.answer('Выберите опцию:')
+    await message.answer('Выберите опцию:', reply_markup=ikb)
 
-@dp.callback_query_handler(text = 'formulas')
+
+@dp.callback_query_handler(text='formulas')
 async def get_formulas(call):
     await call.message.answer('для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5;'
-                                '\nдля женщин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161')
-    # await call.answer()
+                              'для женщин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161.')
+    await call.answer()
 
-@dp.callback_query_handler(text = 'calories')
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+
+@dp.callback_query_handler(text='calories')
 async def set_age(call):
     await call.message.answer('Введите свой возраст:')
-    await call.answer()
     await UserState.age.set()
+    await call.answer()
 
-@dp.message_handler(state = UserState.age)
+
+@dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
     await state.update_data(age=message.text)
     await message.answer('Введите свой рост:')
     await UserState.growth.set()
+
 
 @dp.message_handler(state=UserState.growth)
 async def set_weight(message, state):
@@ -61,21 +65,21 @@ async def set_weight(message, state):
     await message.answer('Введите свой вес:')
     await UserState.weight.set()
 
+
 @dp.message_handler(state=UserState.weight)
-async def send_calories(message, state):
+async def set_weight(message, state):
     await state.update_data(weight=message.text)
     data = await state.get_data()
-    calories = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age'])
-    await message.answer(f"Необходимое количество каллорий: {calories}")
+    result_men = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) + 5
+    result_women = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) - 161
+    await message.answer(f'Ваша норма калорий если вы мужчина {result_men}, если женщина - {result_women}')
     await state.finish()
 
+
 @dp.message_handler()
-async def all_message(message):
-    print('Введите команду /start, чтобы начать общение.')
-    await message.answer("Введите команду /start, чтобы начать общение.")
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
